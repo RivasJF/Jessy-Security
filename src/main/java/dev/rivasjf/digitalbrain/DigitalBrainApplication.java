dd8c040efb413636f69f37b6b08f6b6795072a3d
RivasJF <jonathariflo@gmail.com>
2026-05-18 19:24:47 -0600

    new

---
+++ src/main/java/dev/rivasjf/digitalbrain/DigitalBrainApplication.java
@@ -0,0 +1,13 @@
+package dev.rivasjf.digitalbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalBrainApplication.class, args);
+    }
+
+}
